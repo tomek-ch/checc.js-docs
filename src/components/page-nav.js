b2e0b6e3081b3da99b4edf6f494f7077ff7104eb
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-11-04 22:10:25 +0100

    Add icons to next and prev links

--- src/components/page-nav.js
+++ src/components/page-nav.js
@@ -3,19 +3,26 @@ import React from "react"
 
 import { container, nextLink, linkLabel } from "../styles/PageNav.module.css"
 import { inlineLink } from "../styles/InlineLink.module.css"
+import ArrowRight from "./icons/arrow-right"
+import ArrowLeft from "./icons/arrow-left"
 
 const PageNav = ({ prev, next }) => {
   return (
     <nav className={container}>
       {prev && (
         <Link to={prev.path}>
-          <div className={linkLabel}>← Previous</div>
+          <div className={linkLabel}>
+            <ArrowLeft size="14" />
+            Previous
+          </div>
           <div className={inlineLink}>{prev.title}</div>
         </Link>
       )}
       {next && (
         <Link to={next.path} className={nextLink}>
-          <div className={linkLabel}>Next →</div>
+          <div className={linkLabel}>
+            Next <ArrowRight size="14" />
+          </div>
           <div className={inlineLink}>{next.title}</div>
         </Link>
       )}
