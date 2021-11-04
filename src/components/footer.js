2a8a47ffb0632fc2d4253c3cd54714f28d024228
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-11-04 21:37:56 +0100

    Remove borders

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,15 @@
 import * as React from "react"
 
-import { footerInner, footerOuter } from "../styles/Footer.module.css"
+import { footer } from "../styles/Footer.module.css"
 import { inlineLink } from "../styles/InlineLink.module.css"
 
 const Footer = () => (
-  <div className={footerOuter}>
-    <footer className={footerInner}>
-      © {new Date().getFullYear()}{" "}
-      <a className={inlineLink} href="https://github.com/tomek-ch">
-        Tomasz Chmielnicki
-      </a>
-    </footer>
-  </div>
+  <footer className={footer}>
+    © {new Date().getFullYear()}{" "}
+    <a className={inlineLink} href="https://github.com/tomek-ch">
+      Tomasz Chmielnicki
+    </a>
+  </footer>
 )
 
 export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import {
-  headerOuter,
-  headerInner,
-  nav,
-  hamburgerBtn,
-} from "../styles/Header.module.css"
+import { header, nav, hamburgerBtn } from "../styles/Header.module.css"
 import Check from "./icons/check"
 
 const Header = ({ toggleHamburger, isDocs }) => (
-  <div className={headerOuter}>
-    <header className={headerInner}>
-      <Link to="/">
-        <h1>
-          <Check />
-          Checc.js
-        </h1>
-      </Link>
-      <nav className={nav}>
-        <Link to="/docs/basics/">Docs</Link>
-        <a href="https://github.com/tomek-ch/checc.js">GitHub</a>
-        {isDocs && (
-          <button onClick={toggleHamburger} className={hamburgerBtn}>
-            ≡
-          </button>
-        )}
-      </nav>
-    </header>
-  </div>
+  <header className={header}>
+    <Link to="/">
+      <h1>
+        <Check />
+        Checc.js
+      </h1>
+    </Link>
+    <nav className={nav}>
+      <Link to="/docs/basics/">Docs</Link>
+      <a href="https://github.com/tomek-ch/checc.js">GitHub</a>
+      {isDocs && (
+        <button onClick={toggleHamburger} className={hamburgerBtn}>
+          ≡
+        </button>
+      )}
+    </nav>
+  </header>
 )
 
 export default Header
