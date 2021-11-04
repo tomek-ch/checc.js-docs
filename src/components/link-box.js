024b5978d1ff673b983f0474e683ad123153d4b5
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-11-04 22:17:07 +0100

    Add icons to links

--- src/components/link-box.js
+++ src/components/link-box.js
@@ -2,11 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import ArrowRight from "./icons/arrow-right"
 
-function LinkBox({ title, body, href, to }) {
+function LinkBox({ title, body, href, to, Icon }) {
   const Component = href ? "a" : Link
   return (
     <Component {...(href ? { href } : { to })}>
       <h2>
+        <Icon />
         {title}
         <ArrowRight />
       </h2>
--- src/components/links.js
+++ src/components/links.js
@@ -3,13 +3,19 @@ import links from "../data/links.json"
 import LinkBox from "../components/link-box"
 import { linksContainer } from "../styles/LinksSection.module.css"
 
+import ReactLogo from "./icons/react"
+import ExpressLogo from "./icons/express"
+import Check from "./icons/check"
+
+const icons = { ReactLogo, ExpressLogo, Check }
+
 function Links() {
   return (
     <>
       <h2>Get started with Checc.js</h2>
       <div className={linksContainer}>
         {links.map(link => (
-          <LinkBox key={link.title} {...link} />
+          <LinkBox key={link.title} {...link} Icon={icons[link.icon]} />
         ))}
       </div>
     </>
