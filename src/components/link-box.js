167fad95c5e14318943587bd862b8103393478e4
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-11-04 22:02:59 +0100

    Add arrows to links

--- src/components/link-box.js
+++ src/components/link-box.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
+import ArrowRight from "./icons/arrow-right"
 
 function LinkBox({ title, body, href, to }) {
   const Component = href ? "a" : Link
   return (
     <Component {...(href ? { href } : { to })}>
-      <h2>{title}</h2>
+      <h2>
+        {title}
+        <ArrowRight />
+      </h2>
       <p>{body}</p>
     </Component>
   )
