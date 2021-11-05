c533e773c39ebdebc1c186dcb06561b346affe83
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-11-05 17:41:26 +0100

    Separate link box css component

--- src/components/link-box.js
+++ src/components/link-box.js
@@ -2,11 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import ArrowRight from "./icons/arrow-right"
 import { iconBox } from "../styles/IconBox.module.css"
+import { linkBox } from "../styles/LinkBox.module.css"
 
 function LinkBox({ title, body, href, to, Icon }) {
   const Component = href ? "a" : Link
   return (
-    <Component {...(href ? { href } : { to })}>
+    <Component {...(href ? { href } : { to })} className={linkBox}>
       <h2>
         <div className={iconBox}>
           <Icon />
